b466e81900d612715c96c60e99d06ff8a2cd2760
Gavin Heavyside <gavin@heavyside.co.uk>
2023-08-30 09:17:42 +0100

    Migrate to provided.al2 runtime
    https://aws.amazon.com/blogs/compute/migrating-aws-lambda-functions-from-the-go1-x-runtime-to-the-custom-runtime-on-amazon-linux-2/

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-GOOS=linux GOARCH=amd64 go build && \
-zip usmbot.zip usmbot && \
-rm usmbot && \
+GOOS=linux GOARCH=amd64 go build -tags lambda.norpc -o bootstrap && \
+zip usmbot.zip bootstrap && \
+rm bootstrap && \
 aws --profile $AWS_PROFILE --region eu-west-1 lambda update-function-code --function-name usmbot-mastodon --zip-file fileb://usmbot.zip && \
 rm usmbot.zip
